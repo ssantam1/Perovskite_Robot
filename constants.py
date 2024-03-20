5b632e28714baeb3dabba996fc8cafb43c29faaa
Pierce <94572719+pierce-alvir@users.noreply.github.com>
2024-03-20 19:38:59 -0400

    fixed constants file and added axis file to include 3 axis classes for x y and z axis movement

--- constants.py
+++ constants.py
@@ -5,25 +5,33 @@ Created by Pierce Alvir and Steven Santamorena
 
 '''
 
+#X Constants
 X_STEP_PIN = 18
 X_DIR_PIN = 23
 X_EN_PIN = 24
 X_LIMIT = 1200
-X_MOTOR_VAR = 0.001
+X_STEP_DELAY = 0.001
 
+#Y Constants
 Y_STEP_PIN = 12
 Y_DIR_PIN = 5
 Y_EN_PIN = 6
 Y_LIMIT = 4800
-Y_MOTOR_VAR = 0.0005
+Y_STEP_DELAY = 0.0005
 
+#Z Constants
 Z_STEP_PIN = 16
 Z_DIR_PIN = 20
 Z_EN_PIN = 21
 Z_LIMIT = 3400
-Z_MOTOR_VAR = 0.0008
+Z_STEP_DELAY = 0.0008
 
+#Head Constants
 HEAD_STEP_PIN = 13
 HEAD_DIR_PIN = 19
 HEAD_EN_PIN = 26
 HEAD_VACUUM_PIN = 25
+
+
+
+
---
+++ drivers/axis.py
@@ -0,0 +1,58 @@
+'''
+
+Axis Classes File
+
+Created by Pierce Alvir and Steven Santamorena
+
+Usage: Axis Movement Helper Classes
+
+'''
+import time
+import board
+import RPi.GPIO as GPIO
+import constants
+
+class XAxis(Stepper):
+    def __init__(self, limit: int = None, step_delay: int = None, microstep_mode: int = 1) -> None:
+        step_pin = X_STEP_PIN
+        dir_pin = X_DIR_PIN
+        en_pin = X_EN_PIN
+        limit = limit if limit else X_LIMIT
+        step_delay = step_delay if step_delay else X_STEP_DELAY
+        super().__init__(step_pin, dir_pin, en_pin, limit, step_delay, microstep_mode)
+
+    def right(self, steps: int):
+        self.positive(steps)
+
+    def left(self, steps: int):
+        self.negative(steps)
+
+class YAxis(Stepper):
+     def __init__(self, limit: int = None, step_delay: int = None, microstep_mode: int = 1) -> None:
+        step_pin = Y_STEP_PIN
+        dir_pin = Y_DIR_PIN
+        en_pin = Y_EN_PIN
+        limit = limit if limit else Y_LIMIT
+        step_delay = step_delay if step_delay else Y_STEP_DELAY
+        super().__init__(step_pin, dir_pin, en_pin, limit, step_delay, microstep_mode)
+
+    def inward(self, steps: int):
+        self.positive(steps)
+
+    def outward(self, steps: int):
+        self.negative(steps)
+        
+class ZAxis(Stepper):
+     def __init__(self, limit: int = None, step_delay: int = None, microstep_mode: int = 1) -> None:
+         step_pin = Z_STEP_PIN
+        dir_pin = Z_DIR_PIN
+        en_pin = Z_EN_PIN
+        limit = limit if limit else Z_LIMIT
+        step_delay = step_delay if step_delay else Z_STEP_DELAY
+        super().__init__(step_pin, dir_pin, en_pin, limit, step_delay, microstep_mode)
+
+    def up(self, steps: int):
+        self.positive(steps)
+
+    def down(self, steps: int):
+        self.negative(steps)
