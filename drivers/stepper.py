8a4ea847b0a40c4d451d64d3a4c49267e2977301
Pierce <94572719+pierce-alvir@users.noreply.github.com>
2024-03-20 19:02:04 -0400

    renamed axis file to stepper
    i am going to create a file named axis that will contain the axis classes

--- drivers/stepper.py
+++ drivers/stepper.py
@@ -1,39 +1,17 @@
 '''
 
+Stepper Class File
+
 Created by Pierce Alvir and Steven Santamorena
+
+Usage: Inheritable class for other classes that utilize stepper motors
 
 '''
 import time
 import board
 import RPi.GPIO as GPIO
 
-'''
-These are how to instantiate x, y, and z
-
-X_STEP_PIN = 18
-X_DIR_PIN = 23
-X_EN_PIN = 24
-X_LIMIT = 1200
-X_MOTOR_VAR = 0.001
-
-Y_STEP_PIN = 12
-Y_DIR_PIN = 5
-Y_EN_PIN = 6
-Y_LIMIT = 4800
-Y_MOTOR_VAR = 0.0005
-
-Z_STEP_PIN = 16
-Z_DIR_PIN = 20
-Z_EN_PIN = 21
-Z_LIMIT = 3400
-Z_MOTOR_VAR = 0.0008
-
-x = Axis(X_STEP_PIN,X_DIR_PIN,X_EN_PIN,X_LIMIT,X_MOTOR_VAR)
-y = Axis(Y_STEP_PIN,Y_DIR_PIN,Y_EN_PIN,Y_LIMIT,Y_MOTOR_VAR)
-z = Axis(Z_STEP_PIN,Z_DIR_PIN,Z_EN_PIN,Z_LIMIT,Z_MOTOR_VAR)
-'''
-
-class Axis():
+class stepper():
     def __init__(self, step_pin: int, dir_pin: int, en_pin: int, limit: int, step_delay: int, microstep_mode = 1) -> None:
         '''
         step_pin: int, GPIO pin number for step pin
@@ -67,7 +45,7 @@ class Axis():
         print(f"Moving {steps} steps")
         for _ in range(steps):
             GPIO.output(self.step_pin, 1)
-            time.sleep(0.0005)
+            time.sleep(0.0005) #this was .0003 in the head implementation
 
             GPIO.output(self.step_pin, 0)
             time.sleep(self.step_delay)
