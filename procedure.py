e0b53c5474c78cbc8f3cc05db2cafa134d4a762e
Pierce <94572719+pierce-alvir@users.noreply.github.com>
2024-03-20 17:48:29 -0400

    Update procedure.py

--- procedure.py
+++ procedure.py
@@ -2,6 +2,7 @@ from drivers.axis import Axis
 from drivers.head import Head
 #from drivers.gui import GUI
 from drivers.perovskiteLib import *
+import time
 
 #Constants for Axis Inputs
 X_STEP_PIN = 18
@@ -23,10 +24,29 @@ Z_LIMIT = 3400
 Z_MOTOR_VAR = 0.0008
 
 #Create tuple constants for XYZ coords for things
-
+#PICK_UP_COORDS = (x,y,z)
 #Create instances of the objects
 x = Axis(X_STEP_PIN,X_DIR_PIN,X_EN_PIN,X_LIMIT,X_MOTOR_VAR)
 y = Axis(Y_STEP_PIN,Y_DIR_PIN,Y_EN_PIN,Y_LIMIT,Y_MOTOR_VAR)
 z = Axis(Z_STEP_PIN,Z_DIR_PIN,Z_EN_PIN,Z_LIMIT,Z_MOTOR_VAR)
 p = Head()
 
+def main():
+    while True:
+        print("Type 'demo()' to run pipette & z-axis demo")
+	cmd = input(">> ")
+	try:
+	    exec(cmd)
+	except Exception as E:
+	    print(f"Error {E}, try again.")
+
+
+# Executes whatever commands the user inputs
+if __name__ == "__main__":
+    main()
+
+'''
+help for time stuff
+start = time.perf_counter()
+end = time.perf_counter()
+'''
