c37a47b7babe071ba63045ca33b601777aa3cd0e
Pierce <94572719+pierce-alvir@users.noreply.github.com>
2024-03-20 20:27:30 -0400

    fixed axis to fit the stepper motor change

--- drivers/axis.py
+++ drivers/axis.py
@@ -13,13 +13,14 @@ import RPi.GPIO as GPIO
 import constants
 
 class XAxis(Stepper):
-    def __init__(self, limit: int = None, step_delay: int = None, microstep_mode: int = 1) -> None:
+    def __init__(self, limit: int = None, step_delay1: int = None, step_delay2: int = None, microstep_mode: int = 1) -> None:
         step_pin = X_STEP_PIN
         dir_pin = X_DIR_PIN
         en_pin = X_EN_PIN
         limit = limit if limit else X_LIMIT
-        step_delay = step_delay if step_delay else X_STEP_DELAY
-        super().__init__(step_pin, dir_pin, en_pin, limit, step_delay, microstep_mode)
+        step_delay1 = step_delay1 if step_delay1 else X_STEP_DELAY1
+        step_delay2 = step_delay2 if step_delay2 else X_STEP_DELAY2
+        super().__init__(step_pin, dir_pin, en_pin, limit, step_delay1, step_delay2, microstep_mode)
 
     def right(self, steps: int):
         self.positive(steps)
@@ -28,13 +29,15 @@ class XAxis(Stepper):
         self.negative(steps)
 
 class YAxis(Stepper):
-     def __init__(self, limit: int = None, step_delay: int = None, microstep_mode: int = 1) -> None:
+     def __init__(self, limit: int = None, step_delay1: int = None, step_delay2: int = None, microstep_mode: int = 1) -> None:
         step_pin = Y_STEP_PIN
         dir_pin = Y_DIR_PIN
         en_pin = Y_EN_PIN
         limit = limit if limit else Y_LIMIT
-        step_delay = step_delay if step_delay else Y_STEP_DELAY
-        super().__init__(step_pin, dir_pin, en_pin, limit, step_delay, microstep_mode)
+        step_delay1 = step_delay1 if step_delay1 else Y_STEP_DELAY1
+        step_delay2 = step_delay2 if step_delay2 else Y_STEP_DELAY2
+        super().__init__(step_pin, dir_pin, en_pin, limit, step_delay1, step_delay2, microstep_mode)
+
 
     def inward(self, steps: int):
         self.positive(steps)
@@ -43,13 +46,14 @@ class YAxis(Stepper):
         self.negative(steps)
         
 class ZAxis(Stepper):
-     def __init__(self, limit: int = None, step_delay: int = None, microstep_mode: int = 1) -> None:
+     def __init__(self, limit: int = None, step_delay1: int = None, step_delay2: int = None, microstep_mode: int = 1) -> None:
          step_pin = Z_STEP_PIN
         dir_pin = Z_DIR_PIN
         en_pin = Z_EN_PIN
         limit = limit if limit else Z_LIMIT
-        step_delay = step_delay if step_delay else Z_STEP_DELAY
-        super().__init__(step_pin, dir_pin, en_pin, limit, step_delay, microstep_mode)
+        step_delay1 = step_delay1 if step_delay1 else Z_STEP_DELAY1
+        step_delay2 = step_delay2 if step_delay2 else Z_STEP_DELAY2
+        super().__init__(step_pin, dir_pin, en_pin, limit, step_delay1, step_delay2, microstep_mode)
 
     def up(self, steps: int):
         self.positive(steps)
