e9e811af375fc3acc6c7ce053b35dbda4860ee64
Steven Santamorena <ssantam1@binghamton.edu>
2024-03-21 18:08:05 -0400

    Fix index OOB and return error

--- functionLib.py
+++ functionLib.py
@@ -8,7 +8,7 @@ from drivers.axis import *
 # Function for keeping track and performing xyz movement    
 def zyx(axis: tuple[ZAxis, YAxis, XAxis], coord: tuple[int, int, int], obstacle_det: bool):
     if obstacle_det:
-        zyx(axis, (3200, coord[2], coord[3]), false)
+        zyx(axis, (4000, coord[1], coord[2]), False)
 
     axis_and_coords = [(axis[i], coord[i]) for i in range(len(axis))]
 
@@ -23,15 +23,17 @@ def zyx(axis: tuple[ZAxis, YAxis, XAxis], coord: tuple[int, int, int], obstacle_
     '''
         
     for i in range(3):
-        if (coord[i] >axis[i].limit or coord[i] < 0):
+        if (coord[i] > axis[i].limit or coord[i] < 0):
             print(f"Error")
         elif (coord[i] > axis[i].pos):
+            print("Going positive")
             axis_temp = coord[i]-axis[i].pos
             axis[i].positive(axis_temp)
         else:
+            print("Going negative")
             axis_temp = axis[i].pos-coord[i]
             axis[i].negative(axis_temp)
-    return (z,y,x)
+    return axis
 
 # Function for pipette tip pick up
 
