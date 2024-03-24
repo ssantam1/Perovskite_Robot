b1cdfe146d994ff9a06de773fd2c7494c11b2713
Pierce <94572719+pierce-alvir@users.noreply.github.com>
2024-03-23 21:28:30 -0400

    didn't really do anything to procedure just deleted some useless stuff

--- procedure.py
+++ procedure.py
@@ -4,25 +4,6 @@ from drivers.head import Head
 from drivers.perovskiteLib import *
 import time
 
-#Constants for Axis Inputs
-X_STEP_PIN = 18
-X_DIR_PIN = 23
-X_EN_PIN = 24
-X_LIMIT = 1200
-X_MOTOR_VAR = 0.001
-
-Y_STEP_PIN = 12
-Y_DIR_PIN = 5
-Y_EN_PIN = 6
-Y_LIMIT = 4800
-Y_MOTOR_VAR = 0.0005
-
-Z_STEP_PIN = 16
-Z_DIR_PIN = 20
-Z_EN_PIN = 21
-Z_LIMIT = 3400
-Z_MOTOR_VAR = 0.0008
-
 #Create tuple constants for XYZ coords for things
 #PICK_UP_COORDS = (x,y,z)
 #Create instances of the objects
