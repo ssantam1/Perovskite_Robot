15c2b4b9e0ba7768ab07de71fef786c790879238
Steven Santamorena <ssantam1@binghamton.edu>
2024-03-25 19:57:55 -0400

    Clean up, add comments, remove dead code

--- drivers/carousel.py
+++ drivers/carousel.py
@@ -25,72 +25,42 @@ from constants import *
 
 class Carousel(Stepper):
     '''Class that represents the carousel inside the Perovskite Synthesis System'''
-
     def __init__(self, limit: int = None, step_delay: int = None, microstep_mode: int = 1) -> None:
-        step_pin = CAROUSEL_STEP_PIN
-        dir_pin = CAROUSEL_DIR_PIN
-        en_pin = CAROUSEL_EN_PIN
-        home_pin = None
-        limit = limit if limit else 1200 #random limit that i am putting
+        '''Initializes the carousel stepper motor'''
+        limit = limit if limit else 99999 # arbitrary limit that is never reached
         step_delay = step_delay if step_delay else CAROUSEL_STEP_DELAY
-        super().__init__(step_pin, dir_pin, en_pin, home_pin, limit, step_delay, microstep_mode)
+        super().__init__(CAROUSEL_STEP_PIN, CAROUSEL_DIR_PIN, CAROUSEL_EN_PIN, CAROUSEL_HOME_PIN, limit, step_delay, microstep_mode)
 
         self.steps_per_rev = 200 # Number of steps per revolution on stepper motor
         self.num_vials = 8 # Number of vials/slots in the carousel
-        self.current_vial = 1
+        self.current_vial = 1 # Current vial the carousel is at
 
     def reset_vial(self):
+        '''Tell the carousel that it is at vial 1'''
+        # This is mainly so I can do tests withou turning everything off and on again
         self.current_vial = 1
 
     # Define a method to move the carousel to a given vial
     def move_to_vial(self, target_vial: int) -> None:
         '''Moves the carousel to a desired vial and, sleeps for 3 seconds, and returns to original position'''
-        # Calculate the number of steps needed to move to the desired vial
+        # Check if the target vial is within the bounds of the carousel
         if target_vial < 1 or target_vial > self.num_vials:
             raise Exception("Vial numbers must be non-negative and not exceed num_vials-1")
-    
-        steps_to_move = int(abs(target_vial - self.current_vial) * (self.steps_per_rev / self.num_vials))
 
-        # x  1  2  3  4  5  6  7  8
-        # 1  0  1  2  3  4  5  6  7
-        # 2 -7  0  1  2  3  4  5  6
-        # 3 -6 -7  0  1  2  3  4  5
-        # 4 -5 -6 -7  0  1  2  3  4
-        # 5 -4 -5 -6 -7  0  1  2  3
-        # 6 -3 -4 -5 -6 -7  0  1  2
-        # 7 -2 -3 -4 -5 -6 -7  0  1
+        # Calculate the number of steps needed to move to the desired vial    
+        steps_to_move = int(abs(target_vial - self.current_vial) * (self.steps_per_rev / self.num_vials))
  
-        # Move the stepper motor to the desired position
+        # Set the direction of the motor based on the target vial
         if target_vial > self.current_vial:
             GPIO.output(self.dir_pin,1)
         else:
             GPIO.output(self.dir_pin,0)
-            
+
+        # Move the motor to the target vial
         self.move_steps(steps_to_move)
         self.current_vial = target_vial
-
-    # Define a function to handle user input and move the carousel to the desired vials
-    def move_to_vials(self):
-        '''Takes input sequence from user and calls move_to_vial() for each item'''
-        # Prompt the user for the desired vials
-        vial_sequence: str = input('Enter the desired vials in order (e.g. 1,3,5): ')
-
-        # Split the user input into individual vials
-        vials: list[str] = vial_sequence.split(',')
-
-        # Move the carousel to each desired vial in turn
-        for vial in vials:
-            vial = int(vial)
-
-            # Raise exception if a vial number is too high or too low
-            if vial < 0 or vial > self.num_vials:
-                raise Exception("Vial numbers must be non-negative and not exceed num_vials-1")
-            
-            # Begin rotating carousel
-            self.move_to_vial(vial)
-            time.sleep(1)
 
 if __name__ == "__main__":
     c = Carousel()
     while(True):
-        c.move_to_vials()
+        c.move_to_vial(int(input("Enter a vial number: ")))
