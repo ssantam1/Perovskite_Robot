795f1b46334fac71d4ad21d4138b309b83e6964c
Pierce <94572719+pierce-alvir@users.noreply.github.com>
2024-03-25 10:41:20 -0400

    going to delete but still need to move out the other functions to main

--- functionLib.py
+++ functionLib.py
@@ -5,36 +5,7 @@ Created by Pierce Alvir and Steven Santamorena
 '''
 from drivers.axis import *
 
-# Function for keeping track and performing xyz movement    
-def go_to(axis: tuple[YAxis, XAxis, ZAxis], coord: tuple[int, int, int], obstacle_det: bool):
-    if obstacle_det:
-        axis[2].go_home()
 
-    axis_and_coords = [(axis[i], coord[i]) for i in range(len(axis))]
-
-    '''
-    (axis[1], coord[1]),
-    (axis[2], coord[2]),
-    (axis[3], coord[3])
-
-    for (a, c) in axis_and_coords:
-        a.positive(c)
-        
-    '''
-        
-    for i in range(3):
-        if (coord[i] > axis[i].limit or coord[i] < 0):
-            print(f"Error") # Pierce we really need a more descriptive error and also maybe throw an exception
-            exit()
-        elif (coord[i] > axis[i].pos):
-            print("Going positive")
-            axis_temp = coord[i]-axis[i].pos
-            axis[i].positive(axis_temp)
-        else:
-            print("Going negative")
-            axis_temp = axis[i].pos-coord[i]
-            axis[i].negative(axis_temp)
-    return axis
 
 # Function for pipette tip pick up
 def tip_on(incrementer: int) -> int:
@@ -42,8 +13,9 @@ def tip_on(incrementer: int) -> int:
     incrementer: stored variable in main file that knows what iteration to set the tip to
     function returns incrementer number as well
     '''
+    y_coord, x_coord, z_coord = PIP_TO_TIP
     
-    zyx((z,y,x),(300,1308,13),True) #replace with constant coord
+    go_to((y,x,z),(,1308,13),True)
 
 # Function for pipette tip ejection
 def tip_off():
@@ -52,7 +24,7 @@ def tip_off():
 
         maybe consider washing the tips instead of ejecting
         '''
-	zyx((z,y,x),(1000,100,900),True)
+	go_to((y,x,z),(1000,100,900),True)
 	y.go_home()
 	z.up(1400)
 	z.down(1400)
