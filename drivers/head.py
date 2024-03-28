85c5ecddf70fc09994c328db8e7fcd789d7a6e21
Ssantam1 <ssantam1@binghamton.edu>
2024-03-28 14:07:27 -0400

    Head accuracy fixes

--- drivers/head.py
+++ drivers/head.py
@@ -25,7 +25,7 @@ class Head(Stepper):
         
         self.vacuum_pin = HEAD_VACUUM_PIN # GPIO pin for the vacuum pump
         self.max_uL = HEAD_MAX_UL # Maximum volume of the pipette in microliters
-        self.current_steps = self.max_uL*HEAD_STEPS_PER_UL # Track the current position of the pipette in microliters
+        self.current_steps = 0 # Track the current position of the pipette in microliters
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.vacuum_pin, GPIO.OUT)
@@ -35,40 +35,41 @@ class Head(Stepper):
     def down(self, steps: int):
         '''Moves the pipette plunger down a number of steps'''
         self.positive(steps)
+        self.current_steps += steps
+        print(f"current_steps = {self.current_steps}")
         
     def up(self, steps: int):
         '''Moves the pipette plunger up a number of steps'''
         self.negative(steps)
+        self.current_steps -= steps
+        print(f"current_steps = {self.current_steps}")
 
     # Volume-based pipette actuation functions
         
-    def volume_correction(self, uL: int) -> float:
+    def volume_to_steps(self, uL: int) -> int:
         '''Corrects the volume of the pipette to account for linear error'''
-        return ((uL * UL_CORRECTION_FACTOR) + UL_CORRECTION_OFFSET)
+        corrected_uL = ((uL * UL_CORRECTION_FACTOR) + UL_CORRECTION_OFFSET)
+        return int(HEAD_STEPS_PER_UL * corrected_uL)
         
     def down_uL(self, uL: int):
         '''Moves the pipette plunger down a number of microliters'''
-        uL = self.volume_correction(uL)
-        steps = int(HEAD_STEPS_PER_UL * uL)
+        steps = self.volume_to_steps(uL)
         print(f"Pipette moving {steps} steps ({uL} uL) down...")
         self.down(steps)
-        self.current_steps -= steps
         
     def up_uL(self, uL: int):
         '''Moves the pipette plunger up a number of microliters'''
-        uL = self.volume_correction(uL)
-        steps = int(HEAD_STEPS_PER_UL * uL)
+        steps = self.volume_to_steps(uL)
         print(f"Pipette moving {steps} steps ({uL} uL) up...")
         self.up(steps)
-        self.current_steps += steps
 
     def empty(self):
         '''Empties the pipette of any fluid'''
-        print(f"Emptying pipette of {self.current_steps} steps...")
-        self.down(self.current_steps)
+        steps_to_empty = int(self.max_uL * HEAD_STEPS_PER_UL) - self.current_steps
+        print(f"Emptying pipette of {steps_to_empty} steps...")
+        self.down(steps_to_empty)
         time.sleep(0.2)
-        self.up_uL(self.max_uL)
-        self.current_steps = self.max_uL * HEAD_STEPS_PER_UL
+        self.up(int(self.max_uL * HEAD_STEPS_PER_UL))
     
     # Suction cup functions
 
