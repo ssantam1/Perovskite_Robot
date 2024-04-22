67a8469423933d78827c0ea35eeccc27c2b0339b
Steven Santamorena <ssantam1@binghamton.edu>
2024-04-22 18:48:15 -0400

    Add some comments and docs for axis classes

--- drivers/axis.py
+++ drivers/axis.py
@@ -14,25 +14,48 @@ from constants import *
 from drivers.stepper import Stepper
 
 class Axis(Stepper):
-    def __init__(self, step_pin, dir_pin, en_pin, home_pin, limit, step_delay, flip_dir = False, microstep_mode = 1) -> None:
+    '''
+    Represents a single axis of the gantry
+    '''
+    def __init__(self, step_pin, dir_pin, en_pin, home_pin, limit, step_delay, flip_dir=False, microstep_mode=1) -> None:
+        """
+        Initializes an instance of the Axis class.
+
+        Args:
+            step_pin (int): The GPIO pin number for the step signal.
+            dir_pin (int): The GPIO pin number for the direction signal.
+            en_pin (int): The GPIO pin number for the enable signal.
+            home_pin (int): The GPIO pin number for the home switch.
+            limit (int): The maximum step limit for the axis.
+            step_delay (float): The delay between steps in seconds.
+            flip_dir (bool, optional): Flips all movement dir. Default: False.
+            microstep_mode (int, optional): The microstep factor. Default: 1.
+        """
         super().__init__(step_pin, dir_pin, en_pin, limit, step_delay, flip_dir, microstep_mode)
         self.home_pin = home_pin
-        # You can uncomment this line if you rewire the home switches to use less cabling
-        #GPIO.setup(self.home_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        # Uncomment when home switches are rewired to use internal pull-up
+        # GPIO.setup(self.home_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.home_pin, GPIO.IN)
 
     def is_home(self):
-        # Our switches are active low
+        '''Returns True if axis home switch is pressed, False otherwise'''
+        # Our switches are active low, so we invert here
         return not GPIO.input(self.home_pin)
 
     def go_home(self):
+        '''Homes the axis'''
         print("Going home...")
+        # Move in the "negative" direction until home switch is pressed
         GPIO.output(self.dir_pin, 0 ^ self.flip_dir)
+
         while not self.is_home():
             self.move_steps(1)
             self.pos -= 1
-        print(f"Homed! Steps lost: {self.pos}")
+
+        steps_lost = self.pos
         self.pos = 0
+        print(f"Homed! Steps lost: {self.pos}")
 
 class XAxis(Axis):
     def __init__(self, limit: int = None, step_delay: int = None, microstep_mode: int = 1) -> None:
@@ -101,53 +124,50 @@ class ZAxis(Axis):
         self.positive(steps)
 
 class Gantry():
+    '''Represents the gantry as a whole, with all three axes'''
     def __init__(self, y: YAxis, x: XAxis, z: ZAxis):
         self.x = x
         self.y = y
         self.z = z
     
-    # Function for keeping track and performing xyz movement    
     def go_to(self, coord: tuple[int, int, int], obstacle_det: bool):
         '''
-        coord: take in the coordinated desired via yxz
-        obstacle_det: avoid any obstacles
+        Moves the gantry to the specified coordinates
+        coord: tuple of (y, x, z) coordinates to move to
+        obstacle_det: True if the z-axis should home to avoid obstacles
         '''
-        axis = (self.y,self.x,self.z)
+        axis = (self.y, self.x, self.z)
 
         if obstacle_det:
             axis[2].go_home()
 
-        axis_and_coords = [(axis[i], coord[i]) for i in range(len(axis))]
-
-        '''
-        (axis[1], coord[1]),
-        (axis[2], coord[2]),
-        (axis[3], coord[3])
-
-        for (a, c) in axis_and_coords:
-            a.positive(c)
-            
-        '''
+        # Consider using this instead of the for loop below
+        # axis_and_coords = [(axis[i], coord[i]) for i in range(len(axis))]
+        #
+        # (axis[1], coord[1]),
+        # (axis[2], coord[2]),
+        # (axis[3], coord[3])
+        #
+        # for (axis, coord) in axis_and_coords:
+        #     ...
             
         for i in range(3):
             if (coord[i] > axis[i].limit or coord[i] < 0):
-                print(f"Error") # Pierce we really need a more descriptive error and also maybe throw an exception
-                exit()
+                raise ValueError("Invalid coordinate")
             elif (coord[i] > axis[i].pos):
                 axis_temp = coord[i]-axis[i].pos
                 axis[i].positive(axis_temp)
             else:
                 axis_temp = axis[i].pos-coord[i]
                 axis[i].negative(axis_temp)
-        return axis
+        return self
     
     def home(self):
-        '''
-        Homes all axes
-        '''
+        '''Homes all axes'''
         self.z.go_home()
         self.x.go_home()
         self.y.go_home()
     
     def get_coords(self):
+        '''Returns the current coordinates of the gantry'''
         return (self.y.pos, self.x.pos, self.z.pos)
